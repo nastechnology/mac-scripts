c2c3d954436041111d9956a66d20cdd4f27f180c
Mark Myers <marcusmyers@gmail.com>
2014-01-11 09:50:41 -0500

    Fixed restoreImage.sh to use new AutoDMG Image

--- restoreImage.sh
+++ restoreImage.sh
@@ -3,7 +3,12 @@
 SRC=$1
 DST=Macintosh\ HD\ 1
 
-asr restore --source /Volumes/Macintosh\ HD/Users/markmyers/Documents/Projects/NACS-MAC-Images/"$SRC" --target /Volumes/"$DST" --noverify --verbose --erase --noprompt
+if [ -z "$1" ]; then
+echo "No arguments supplied - Please provide an image name i.e. ./restoreImage.sh ~/Documents/MacBook_image.dmg"
+  exit 1
+fi
+
+asr restore --source $1 -target /Volumes/"$DST" -erase -noverify
 
 echo Done with imaging
 echo Please reboot this Mac and
