6c6342b8785027a50197a2bb560a9aec822fe827
Mark Myers <marcusmyers@gmail.com>
2014-10-15 10:43:49 -0400

    Added content to the restoreImage.sh file to tell if a new mac is plugged in then to go ahead and image it

--- restoreImage.sh
+++ restoreImage.sh
@@ -8,8 +8,25 @@ echo "No arguments supplied - Please provide an image name i.e. ./restoreImage.s
   exit 1
 fi
 
-asr restore --source $1 -target /Volumes/"$DST" -erase -noverify
+while true
+do
 
-echo Done with imaging
-echo Please reboot this Mac and
-echo move on to your next imaging station
+if [ -d "/Volumes/Macintosh HD 1" ]; then
+  asr restore --source $1 -target /Volumes/"$DST" -erase -noverify
+
+  echo "Done with imaging"
+  echo "Please reboot this Mac and"
+  echo "move on to your next imaging station"
+  diskutil unmount "/Volumes/Macintosh HD 1"
+  echo -e '\a'
+
+  say -v  Fred "Mac is imaged"
+
+  echo -e '\a'
+
+fi
+
+echo "Press [Ctrl+C] to stop.."
+sleep 1
+
+done
